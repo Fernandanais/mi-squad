35cab3d4462c8da34108090e40d1b708e6724b03
Fernanda Zamora <fernandanais@gmail.com>
2017-05-05 07:27:54 -0300

    Agregamos primeros archivos

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function MiembroSquad( nombre, edad, hobbie){
+	this.nombre = nombre,
+	this.edad = edad,
+
+}
+
+var Vale = new MiembroSquad("Valentina Miranda", "25", );
+var Naty = new MiembroSquad("Natalia Villalobos", "30", "tocar batería");
+var Espe = new MiembroSquad("Esperanza Lucero", "25", "fotografía", "leer", "dormir");
+var Diana = new MiembroSquad("Diana Villasmil", "29", "cantar", "ver series coreanas", "leer");
+var Cindy = new MiembroSquad("Cindialy Berrios", "23", "Trekking", "Escuchar música", "Deporte");
+var Fernanda = new MiembroSquad("Fernanda Zamora", "30", "Escuchar música", "Hacer fotografías", "Pensar en el existencialismo");
